5ff8f5dc99c2692186bd48053d3e24100ce56706
All Gas No Brakes <eleanor.miley.19@cnu.edu>
2021-11-30 18:39:19 -0500

    added to counter.php

--- html/counter.php
+++ html/counter.php
@@ -10,17 +10,28 @@ $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 $value = $row["Customer_count"];
 
+
+if(isset(["increase"])) {
+	$sql1="insert into customers values ($result++);"
+	$conn->query($sql1)
+}
+
+
+
+
 // if(isset(["inc"])) {
 // sql code
 // $conn->query($sql code variable)
-
+echo $sql;
+echo $sql1;
 
 ?>
 <form method="post">
-	<input type="submit" value="createname">
+	<input type="submit" value="increase">
 	<input type="submit" value="createname">
 	<input type="submit" value="createname">
 </form>
+
 </html>
 
 

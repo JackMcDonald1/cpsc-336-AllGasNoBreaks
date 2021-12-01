0654137850711b1d9ac9e0a13c2b8c667f9f376f
patricktehan <patrick.tehan.19@cnu.edu>
2021-11-30 20:59:19 -0500

    updated buttons along with fixing format of wepage

--- html/counter.php
+++ html/counter.php
@@ -9,27 +9,37 @@ include 'connect.php';
 
 //$row = $result->fetch_assoc();
 //$value = $row["customer_count"];
-//$counter = 0;
+
 
 if(isset($_POST["button"])) {
-	//$insert = "Insert into customers values ($value++)";
+//	$insert = "Insert into customers values ($value++)";
 	$insert = "update customers set customer_count = customer_count+1";
-	echo $value;
 	$conn->query($insert);
 }
 
+if(isset($_POST["subtract"]))	{
+	$insert = "update customers set customer_count = customer_count-1";
+	$conn->query($insert);
+}
 
+if(isset($_POST["reset"]))	{
+	$insert = "update customers set customer_count=0";
+	$conn->query($insert);
+}
 
+//$sql = "insert into customers value (2)";
 
 // if(isset(["inc"])) {
 // sql code
 // $conn->query($sql code variable)
 
 ?>
-<form method="POST">
+
+
+<form>
 	<input type="submit" name = "button" value="ADD">
-	<input type="submit" value="createname">
-	<input type="submit" value="createname">
+	<input type="submit" name="subtract" value="subtract">
+	<input type="submit" name="reset" value="reset">
 </form>
 
 </html>

f2d8ae6792517f4e5c6a9b0236f499364ed1b85b
patricktehan <patrick.tehan.19@cnu.edu>
2021-12-02 22:05:40 -0500

    fixed counter to display the correct number

--- html/counter.php
+++ html/counter.php
@@ -4,34 +4,27 @@
 include 'connect.php';
 $sql = "select customer_count from customers";
 
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
 
-echo "<br>Total customer count is: " .$row["customer_count"]. "<br>";
 	
 if(isset($_POST["INSERT"])) {
 	$insert = "update customers set customer_count = customer_count+1";
 	$ins = $conn->query($insert);
-//	echo "<br> Total customer count is: " .$row["customer_count"]."<br>";
 }
 			
 
 if(isset($_POST["subtract"]))	{
 	$subtract = "UPDATE customers SET customer_count = customer_count-1";
 	$sub = $conn->query($subtract);
-				
-//	echo "<br> Total customer count is: " .$row["customer_count"]."<br>";
 }
 
 if(isset($_POST["reset"]))	{
 	$reset = "UPDATE customers SET customer_count=0";
 	$res = $conn->query($reset);
-//	echo "<br> Total customer count is: " .$row["customer_count"]."<br>";
 }
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+echo "<br> Total customer count is: " .$row["customer_count"]."<br>";
 
-//if(isset($_POST["INSERT"]) || isset($_POST["subtract"]) || isset($_POST["reset"])) {
-//	echo "<br> Total customer count is: " .$row["customer_count"]."<br>";
-//}
 ?>
 
 
